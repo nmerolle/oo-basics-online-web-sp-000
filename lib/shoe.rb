aa417c0d028d573eae5a647ef13c35f0a9e53767
Nicholas Merolle <nmerolle11@hotmail.com>
2020-09-27 18:38:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,7 +9,7 @@ class Shoe
   end
   
   def cobbler
-    puts "says that the shoe has been repaired"
-    shoe.condition = "new"
+    puts "Your shoe is as good as new!"
+    self.condition = "new"
   end
 end
